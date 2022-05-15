449de3119db092f01d0582f22411f40d7c066740
lakshya <erenpool7214@gmail.com>
2022-05-15 14:51:58 +0530

    Added create and read students

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const mongoose=require('mongoose')
+const {mongoose}=require('mongoose')
 const app=express();
 const port=80;
 
-mongoose.connect(`mongodb://localhost:27017/project`,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
+mongoose.connect(`mongodb+srv://Lakshya_roonwal:wTnISnRNXgiGdKpJ@cluster0.5uao7.mongodb.net/schoolproject?retryWrites=true&w=majority`,{useNewUrlParser:true,useUnifiedTopology:true},(err)=>{
     if(!err) console.log(`connected db`);
     else console.log(`db error`);
 })
 const studentSchema=new mongoose.Schema({
     name:String,
     fathername:String,
-    motername:String,
-    class:Number,
-    address:Number,
+    mothername:String,
+    address:String,
+    class:String,
     contactno:Number
 })
-const studentModel=new mongoose.model('students',studentSchema);
+const studentModel=new mongoose.model('student',studentSchema);
 
 
 
@@ -27,6 +27,10 @@ app.post('/addstudent',(req,res)=>{
     const addstudent=studentModel(req.body)
     addstudent.save();
     res.json({msg:"Studnet added"})
+})
+app.get('/getallstudents',async(req,res)=>{
+    const students=await studentModel.find();
+    res.json(students);
 })
 
 app.listen(port,()=>{
